d369d9842fc00977846ba541ceff9224c99b16d3
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-07-27 19:59:04 +0100

    created initial page

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import initialPage from './scripts/pageload'
+
+initialPage.init()
---
+++ src/scripts/pageload.js
@@ -0,0 +1,36 @@
+import '../styles/main.css'
+
+const initialPage = (
+    function() {
+        const body = document.querySelector('body');
+        const content = document.querySelector('div#content');
+        const homeCard = document.createElement('div');
+        homeCard.setAttribute('id', "homeCard")
+        const homeHeader = document.createElement('div');
+        homeHeader.setAttribute('id', 'Homeheader')
+        const headerText = document.createElement('p');
+        headerText.textContent = "Le FrostWalker's";
+        homeHeader.appendChild(headerText);
+        const homeMain = document.createElement('div');
+        homeMain.setAttribute('id', 'Homemain');
+        const mainText = document.createElement('p');
+        mainText.textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Nulla imperdiet enim eros, id dignissim est pharetra vestibulum. In convallis, 
+        felis ut blandit tempus, nulla ipsum rhoncus mauris, in gravida mauris arcu ac 
+        est. Fusce molestie neque nec tortor lobortis porttitor. Pellentesque ac 
+        fermentum lorem. Vivamus et blandit libero. Proin condimentum egestas metus 
+        nec condimentum. Nulla facilisi. Vivamus diam massa, dignissim sed finibus at, 
+        pellentesque eget sem.`;
+        homeMain.appendChild(mainText);
+        homeCard.appendChild(homeHeader);
+        homeCard.appendChild(homeMain);
+        const init = () => {
+            content.appendChild(homeCard);
+            body.classList.add('backgroundimage');
+            content.appendChild(homeCard);
+        }
+        return { init }
+    }
+)()
+
+export default initialPage
