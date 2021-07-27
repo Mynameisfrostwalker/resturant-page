42eeae9049f4edddab968ea5f8787184924fba9d
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-07-27 19:57:02 +0100

    added html and css plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: '.src/index.js',
+    entry: './src/index.js',
     devtool: 'inline-source-map',
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+            favicon: './src/images/favicon.ico',
+        }),
+        new MiniCssExtractPlugin(),
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
     module: {
         rules: [{
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
