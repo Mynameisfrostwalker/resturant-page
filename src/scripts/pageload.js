c7ba87a33e24cf2a36333faf835b7cc1557948bb
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-07-28 18:47:06 +0100

    created initial page

--- src/scripts/pageload.js
+++ src/scripts/pageload.js
@@ -1,9 +1,33 @@
 import '../styles/main.css'
 
-const initialPage = (
+const navCreate = (
     function() {
-        const body = document.querySelector('body');
-        const content = document.querySelector('div#content');
+        const tab = document.createElement('div');
+        tab.setAttribute('id', 'tab-content');
+        const tabUl = document.createElement('ul');
+        const tab1li = document.createElement('li');
+        tab1li.setAttribute('id', 'tab1li');
+        tab1li.textContent = 'Home';
+        const tab2li = document.createElement('li');
+        tab2li.setAttribute('id', 'tab2li');
+        tab2li.textContent = 'Menu';
+        const tab3li = document.createElement('li');
+        tab3li.setAttribute('id', 'tab3li');
+        tab3li.textContent = 'Contact';
+        const create = () => {
+            tab1li.style.color = 'red';
+            tabUl.appendChild(tab1li);
+            tabUl.appendChild(tab2li);
+            tabUl.appendChild(tab3li);
+            tab.appendChild(tabUl)
+        }
+        return { create, tab };
+    }
+)()
+
+
+const main = (
+    function() {
         const homeCard = document.createElement('div');
         homeCard.setAttribute('id', "homeCard")
         const homeHeader = document.createElement('div');
@@ -21,13 +45,25 @@ const initialPage = (
         fermentum lorem. Vivamus et blandit libero. Proin condimentum egestas metus 
         nec condimentum. Nulla facilisi. Vivamus diam massa, dignissim sed finibus at, 
         pellentesque eget sem.`;
-        homeMain.appendChild(mainText);
-        homeCard.appendChild(homeHeader);
-        homeCard.appendChild(homeMain);
+        const create = () => {
+            homeMain.appendChild(mainText);
+            homeCard.appendChild(homeHeader);
+            homeCard.appendChild(homeMain);
+        }
+        return { create, homeCard }
+    }
+)()
+
+const initialPage = (
+    function() {
+        const body = document.querySelector('body');
+        const content = document.querySelector('div#content');
         const init = () => {
-            content.appendChild(homeCard);
+            navCreate.create();
+            main.create();
+            content.appendChild(navCreate.tab);
             body.classList.add('backgroundimage');
-            content.appendChild(homeCard);
+            content.appendChild(main.homeCard);
         }
         return { init }
     }
